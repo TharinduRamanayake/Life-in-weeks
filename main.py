6c338227df8bf7404dac77af80b5724d9c2a43de
J.M.  Tharindu Ramanayake <70686843+TharinduRamanayake@users.noreply.github.com>
2023-05-13 19:22:51 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# 🚨 TharinduRamanayake 👇
+age = input("What is your current age? ")
+
+until = (90 - int(age))
+
+days = int (until * 365)
+weeks = int (until * 52)
+years = int (until * 12)
+
+print (f"You have {days} days, {weeks} weeks, and {years} months left.")
+
+
+
+
+
